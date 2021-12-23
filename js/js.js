30cabf12d8dabdaeb31510b47ea99e4f2f2cfc09
danilhautilus911 <danil.matlahov@yandex.ru>
2021-12-23 19:55:10 +0700

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,5 +1,12 @@
-$("#callme").click(function() {
-    $('.o_brende').animate({
-        scrollTop: $(".second_page").offset().top
-    }, 1000);
+$(function() {
+  $('.menu').each(function() {
+    $(this).on('click', function(e) {
+      e.preventDefault();
+      let scrollTo = $(this).attr('href');
+      let whitespace = scrollTo == '#s1' ? 70:30;
+      $('html.body').animate({
+        scrollTop: $(scrollTo).offset().top - whitespace
+      }, 1000);
+    });
+  });
 });
